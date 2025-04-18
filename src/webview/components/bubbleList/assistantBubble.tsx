3fde681a9aba3e1597276dec2599e46df699c3f9
chunpeng <chunpeng.yu@outlook.com>
2025-04-18 19:19:17 +0800

    feat(assistantBubble): 添加对消息内容的分类渲染功能
    引入 `Accordion` 组件和 `useMemoizedFn` 钩子，实现对消息内容的分类型渲染。现在可以分别处理 `reasoning` 和 `text` 类型的消息内容，提升用户体验。

--- src/webview/components/bubbleList/assistantBubble.tsx
+++ src/webview/components/bubbleList/assistantBubble.tsx
@@ -1,13 +1,29 @@
 import { Message } from 'ai';
-import React from 'react';
+import { Accordion, AccordionItem } from '@heroui/accordion';
 import { Brain } from 'lucide-react';
 import MarkdownCom from '../markdown';
+import { useMemoizedFn } from 'ahooks';
 
 interface AssistantBubbleProps {
   message: Message;
 }
 export default function AssistantBubble(props: AssistantBubbleProps) {
   const { message } = props;
+
+  const renderContent = useMemoizedFn((content) => {
+    return <MarkdownCom>{content}</MarkdownCom>;
+  });
+
+  const renderReasoning = useMemoizedFn((reasoning) => {
+    return (
+      <Accordion isCompact variant="bordered">
+        <AccordionItem key="1" aria-label="深度思考" title="深度思考">
+          {reasoning}
+        </AccordionItem>
+      </Accordion>
+    );
+  });
+
   return (
     <div>
       <div className="inline-flex gap-2">
@@ -15,7 +31,14 @@ export default function AssistantBubble(props: AssistantBubbleProps) {
         ArtiCode
       </div>
       <div className="border-t-1 pt-1">
-        <MarkdownCom>{message.content}</MarkdownCom>
+        {message.parts?.map((item) => {
+          if (item.type === 'reasoning') {
+            return renderReasoning(item.reasoning);
+          }
+          if (item.type === 'text') {
+            return renderContent(item.text);
+          }
+        })}
       </div>
     </div>
   );
